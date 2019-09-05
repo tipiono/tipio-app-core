280a6fc53e643ad50545ba4e7567c323eff168c7
Ismajl Ramadani <ismajl.ramadani@ornio.no>
2019-09-05 16:13:20 +0200

    docs

--- src/stories/Customer/Modals/ImageCropper.stories.js
+++ src/stories/Customer/Modals/ImageCropper.stories.js
@@ -40,6 +40,36 @@ storiesOf('Customer/Components/Modals/ImageCropper', module)
             <div className="col-3 m-3">
                 <h2>Image Cropper</h2>
                 <CropperDemo />
+                <pre>
+                    {`
+    const [showCropper, setShowCropper] = useState(false);
+    const [selectedFile, setSelectedFile] = useState(undefined);
+    const [fileInput, setFileInput] = useState(undefined);
+
+    const handleSelectedFile = e => {
+        const file = e.target.files[0];
+        file.preview = URL.createObjectURL(file);
+        setSelectedFile(file);
+        setShowCropper(true);
+    };
+    return(
+        <>
+            <form className="avatar-uploader-btn">
+                 <input type="file" name="" id="" onChange={handleSelectedFile} ref={(input) => {
+                    setFileInput(input);
+                 }}/>
+                { showCropper &&
+                    <ImageCropper
+                        aspectRatio={1}
+                        image={selectedFile}
+                        onDone={action('done')}
+                        onClose={action('close')}
+                    /> }
+            </form>
+        </>
+    )
+                    `}
+                </pre>
             </div>
         </>
     )
