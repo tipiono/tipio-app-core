85c97502aaa75648af5af262baeeef709e94bf8f
Ismajl Ramadani <ismajl.ramadani@ornio.no>
2019-09-16 16:52:56 +0200

    Tab component

--- src/stories/UI/Tabs/Tabs.stories.js
+++ src/stories/UI/Tabs/Tabs.stories.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import {storiesOf} from '@storybook/react';
-import {Tabs, PillTabs} from '../../../lib'
+import {Tabs, TabNav, TabNavItem, TabContent, TabContentItem, PillTabs} from '../../../lib'
 
 storiesOf('UI/Tabs', module)
     .add('Tabs', () =>
         <>
             <div className="col-5 m-3">
-             <Tabs />
+             <Tabs>
+                 <TabNav>
+                     <TabNavItem>Interessert</TabNavItem>
+                     <TabNavItem>Motatte tilbud</TabNavItem>
+                     <TabNavItem>Bestillinger</TabNavItem>
+                 </TabNav>
+                 <TabContent>
+                     <TabContentItem>Test 1 content</TabContentItem>
+                     <TabContentItem>Test 2 content</TabContentItem>
+                     <TabContentItem>Test 3 content</TabContentItem>
+                 </TabContent>
+            </Tabs>
             </div>
         </>
     )
