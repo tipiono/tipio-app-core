8872fdc370f1662a7edca9735ad69657445b5fc6
Ismajl Ramadani <ismajl.ramadani@ornio.no>
2019-09-30 13:57:17 +0200

    Image Slider

--- src/lib/UI/ImageSlider/ImageSlider.js
+++ src/lib/UI/ImageSlider/ImageSlider.js
@@ -1,13 +1,27 @@
 import React from 'react';
+import { Carousel } from 'react-responsive-carousel';
 
-function ImageSlider({}) {
-    return (
-        <>
-            <div>
-                ###
-            </div>
-        </>
-    )
+function ImageSlider({
+    images,
+    showThumbs,
+}) {
+    return <>
+        <div className="tipio-gallery">
+            <Carousel
+                showIndicators
+                showThumbs={showThumbs}
+                showArrows={showThumbs}
+            >
+                {images && images.map((item) => (
+                    <div>
+                        <img src={item.blob_url} alt="..." />
+                        {/* <div className="position-absolute"></div> */}
+                        {/* <TipioCountdown expires_in={tipio.expires_in} /> */}
+                    </div>
+                ))}
+            </Carousel>
+        </div>
+    </>
 }
 
 export default ImageSlider;
