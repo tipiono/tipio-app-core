20bce02a5b4475aee66009acced32eb93f9577f4
Ismajl Ramadani <ismajl.ramadani@ornio.no>
2019-09-30 21:53:33 +0200

    form to input

--- dist/UI/Forms/FromToInput.js
+++ dist/UI/Forms/FromToInput.js
@@ -10,11 +10,13 @@ var _react = _interopRequireDefault(require("react"));
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 function _default(_ref) {
-  var label = _ref.label;
+  var from = _ref.from,
+      to = _ref.to,
+      label = _ref.label;
   return _react.default.createElement(_react.default.Fragment, null, _react.default.createElement("label", {
     className: "pure-material-textfield-outlined from-to-input"
   }, _react.default.createElement("input", {
-    placeholder: "50",
+    placeholder: to,
     type: "number",
     id: "subdomain"
   }), _react.default.createElement("label", {
@@ -23,5 +25,5 @@ function _default(_ref) {
     className: "inside-label"
   }, "Fra"), _react.default.createElement("p", {
     className: "static-placeholder"
-  }, "1 -")));
+  }, from, " -")));
 }
