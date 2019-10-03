b096e89883fb9378862b203b2c2ff21f0760ca4c
Ismajl Ramadani <ismajl.ramadani@ornio.no>
2019-10-03 17:53:08 +0200

    radio button props

--- dist/UI/Forms/RadioButton.js
+++ dist/UI/Forms/RadioButton.js
@@ -9,21 +9,18 @@ var _react = _interopRequireDefault(require("react"));
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-function _objectDestructuringEmpty(obj) { if (obj == null) throw new TypeError("Cannot destructure undefined"); }
-
 function RadioButton(_ref) {
-  _objectDestructuringEmpty(_ref);
-
+  var id = _ref.id,
+      text = _ref.text;
   return _react.default.createElement(_react.default.Fragment, null, _react.default.createElement("div", {
     className: "md-radio"
   }, _react.default.createElement("input", {
-    id: "1",
+    id: id,
     type: "radio",
-    name: "g",
-    checked: true
+    name: id
   }), _react.default.createElement("label", {
-    htmlFor: "1"
-  }, "Option 1")));
+    htmlFor: id
+  }, text)));
 }
 
 var _default = RadioButton;
