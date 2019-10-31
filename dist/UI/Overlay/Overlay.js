ecd210c7a2a34fed335d74bb1b38b98a4e625694
Ismajl Ramadani <ismajl.ramadani@ornio.no>
2019-10-31 11:22:58 +0100

    TIPIO-317

--- dist/UI/Overlay/Overlay.js
+++ dist/UI/Overlay/Overlay.js
@@ -5,12 +5,19 @@ Object.defineProperty(exports, "__esModule", {
 });
 exports.default = void 0;
 
-var _react = _interopRequireDefault(require("react"));
+var _react = _interopRequireWildcard(require("react"));
+
+var _useLockBodyScroll = _interopRequireDefault(require("../../Hooks/useLockBodyScroll"));
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
+function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } else { var newObj = {}; if (obj != null) { for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) { var desc = Object.defineProperty && Object.getOwnPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : {}; if (desc.get || desc.set) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } } newObj.default = obj; return newObj; } }
+
 var Overlay = function Overlay(_ref) {
-  var _onClick = _ref.onClick;
+  var _onClick = _ref.onClick,
+      opacity = _ref.opacity,
+      top = _ref.top;
+  (0, _useLockBodyScroll.default)();
   return _react.default.createElement("a", {
     onClick: function onClick(e) {
       e.preventDefault();
@@ -18,7 +25,11 @@ var Overlay = function Overlay(_ref) {
       _onClick();
     }
   }, _react.default.createElement("div", {
-    className: "overlay show"
+    className: "overlay show",
+    style: {
+      opacity: opacity,
+      top: top || 0
+    }
   }));
 };
 
