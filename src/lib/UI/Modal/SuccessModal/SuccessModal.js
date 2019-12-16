5df1724041a63e7eb5234b971d44c6d69f85d13c
Bardh Kryeziu <you@example.com>
2019-12-16 10:43:16 +0100

    animation .gif on thank you for order modal

--- src/lib/UI/Modal/SuccessModal/SuccessModal.js
+++ src/lib/UI/Modal/SuccessModal/SuccessModal.js
@@ -6,20 +6,7 @@ function SuccessModal({successTitle, successDescription, ...props}) {
         <>
             <Modal {...props}>
                 <div className="successModal">
-                    <svg className="successModal--icon" width={75} height={75}>
-                        <g fill="none" fillRule="evenodd" strokeWidth={4}>
-                            <path
-                                stroke="#4BBBAB"
-                                d="M64.702 23.398a33.224 33.224 0 014.049 15.946c0 18.434-14.943 33.376-33.375 33.376C16.943 72.72 2 57.778 2 39.344 2 20.912 16.943 5.969 35.376 5.969a33.215 33.215 0 0118.589 5.653"
-                            />
-                            <path
-                                stroke="#E44498"
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                d="M21.554 31.356L35.375 49.33 72.56 2.629"
-                            />
-                        </g>
-                    </svg>
+                    <img className="successModal--animation" src="https://tipio.ams3.digitaloceanspaces.com/staging/public/020s0040_1280x1280_wAlpha.gif" alt=""/>
 
                     <h3 className="successModal--title">{successTitle}</h3>
                     <p className="successModal--description">{successDescription}</p>
