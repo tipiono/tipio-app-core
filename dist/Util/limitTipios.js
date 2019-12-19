5608712ad652aebf5ad6deea5a96e6d2f81d313c
Rinor Sadiku <rinor.sadiku@ornio.no>
2019-12-19 15:12:03 +0100

    Added limitTipios function to utils

---
+++ dist/Util/limitTipios.js
@@ -0,0 +1,12 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
+
+var _default = function _default(tipios, length) {
+  return tipios.slice(0, length);
+};
+
+exports.default = _default;
