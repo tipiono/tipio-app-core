53457fb10028bdbd12d5b7d8935cf904b2fd4e72
Rinor Sadiku <rinor.sadiku@ornio.no>
2020-01-14 13:10:06 +0100

    TIPIO-532: Added Footer Card at the bottom of Tipio Bind page

--- dist/Customer/Cards/FooterMainCard/FooterMainCard.js
+++ dist/Customer/Cards/FooterMainCard/FooterMainCard.js
@@ -7,6 +7,8 @@ exports.default = void 0;
 
 var _react = _interopRequireDefault(require("react"));
 
+var _propTypes = _interopRequireDefault(require("prop-types"));
+
 var _addonActions = require("@storybook/addon-actions");
 
 var _addonKnobs = require("@storybook/addon-knobs");
@@ -15,16 +17,27 @@ var _SecondaryButton = _interopRequireDefault(require("../../../UI/Buttons/Secon
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-var FooterMainCard = function FooterMainCard() {
+var FooterMainCard = function FooterMainCard(_ref) {
+  var title = _ref.title,
+      marketPrice = _ref.marketPrice,
+      firstRange = _ref.firstRange,
+      secondRange = _ref.secondRange,
+      bindCount = _ref.bindCount,
+      onBindClick = _ref.onBindClick;
+
+  var generateNewPrice = function generateNewPrice() {
+    return bindCount >= 0 && bindCount >= firstRange.from && bindCount <= firstRange.to ? firstRange.price : secondRange.price;
+  };
+
   return _react.default.createElement("div", {
     className: "footerMainCard"
   }, _react.default.createElement("div", {
     className: "row align-items-center"
   }, _react.default.createElement("div", {
-    className: "col-6 d-none d-md-block"
+    className: "col-5 d-none d-md-block"
   }, _react.default.createElement("h3", {
     className: "footerMainCard__title"
-  }, "Productname")), _react.default.createElement("div", {
+  }, title)), _react.default.createElement("div", {
     className: "col-8 col-md-4"
   }, _react.default.createElement("div", {
     className: "footerMainCard__content"
@@ -38,16 +51,24 @@ var FooterMainCard = function FooterMainCard() {
     className: "footerMainCard__content__priceValue"
   }, _react.default.createElement("h6", {
     className: "footerMainCard__content__priceValue__new"
-  }, "7 990 Kr"), _react.default.createElement("h6", {
+  }, generateNewPrice(), " Kr"), _react.default.createElement("h6", {
     className: "footerMainCard__content__priceValue__first"
-  }, "11 070 Kr")))), _react.default.createElement("div", {
-    className: "col-4 col-md-2"
+  }, marketPrice, " Kr")))), _react.default.createElement("div", {
+    className: "col-4 col-md-3"
   }, _react.default.createElement(_SecondaryButton.default, {
-    onClick: (0, _addonActions.action)('clicked'),
-    text: (0, _addonKnobs.text)('Label', 'Kjøp nå'),
+    onClick: onBindClick,
+    text: (0, _addonKnobs.text)('Label', 'Legg i handlekurv'),
     loading: (0, _addonKnobs.boolean)('Loading ', false)
   }))));
 };
 
+FooterMainCard.propTypes = {
+  title: _propTypes.default.string.isRequired,
+  marketPrice: _propTypes.default.string.isRequired,
+  firstRange: _propTypes.default.object.isRequired,
+  secondRange: _propTypes.default.object.isRequired,
+  bindCount: _propTypes.default.number.isRequired,
+  onBindClick: _propTypes.default.func.isRequired
+};
 var _default = FooterMainCard;
 exports.default = _default;
