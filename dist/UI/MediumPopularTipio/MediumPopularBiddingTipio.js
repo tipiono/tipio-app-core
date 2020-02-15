c915e1cef4ce3a169b4d693591d5fa2d713dde10
Rinor Sadiku <rinor.sadiku@ornio.no>
2020-02-15 22:39:42 +0100

    Fixed issue around the current price of the tipio

--- dist/UI/MediumPopularTipio/MediumPopularBiddingTipio.js
+++ dist/UI/MediumPopularTipio/MediumPopularBiddingTipio.js
@@ -9,9 +9,11 @@ var _react = _interopRequireDefault(require("react"));
 
 var _MediumPopularTipio = _interopRequireDefault(require("./MediumPopularTipio"));
 
-var _reactI18next = require("react-i18next");
-
 var _SecondaryButton = _interopRequireDefault(require("../Buttons/SecondaryButton"));
+
+var _generatePrice = _interopRequireDefault(require("../../Util/generatePrice"));
+
+var _reactI18next = require("react-i18next");
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
@@ -20,15 +22,15 @@ var MediumPopularBiddingTipio = function MediumPopularBiddingTipio(props) {
       t = _useTranslation.t;
 
   var rangeList = props.tipio_offer && props.tipio_offer.offer_price_ranges || [];
-  var firstRange = rangeList.length > 0 && rangeList[0]; // const secondRange = rangeList.length > 1 && parseInt(rangeList[1].price, 10) && rangeList[1];
-
+  var firstRange = rangeList.length > 0 && parseInt(rangeList[1].price, 10) && rangeList[1];
+  var secondRange = rangeList.length > 0 && parseInt(rangeList[0].price, 10) && rangeList[0];
   return _react.default.createElement(_MediumPopularTipio.default, props, _react.default.createElement("div", {
     className: "mediumPopularTipio__content__price"
   }, _react.default.createElement("h6", {
     className: "mediumPopularTipio__content__price--marketprice"
   }, props.market_price, " Kr"), _react.default.createElement("h6", {
     className: "mediumPopularTipio__content__price--saleprice"
-  }, firstRange.price, " Kr")), _react.default.createElement("div", {
+  }, (0, _generatePrice.default)(props.bind_count, firstRange, secondRange), " Kr")), _react.default.createElement("div", {
     className: "mediumPopularTipio__content__submit"
   }, props.binded ? _react.default.createElement(_SecondaryButton.default, {
     text: "Del n\xE5",
--- src/lib/UI/MediumPopularTipio/MediumPopularBiddingTipio.js
+++ src/lib/UI/MediumPopularTipio/MediumPopularBiddingTipio.js
@@ -1,20 +1,21 @@
 import React from 'react';
 import MediumPopularTipio from "./MediumPopularTipio";
-import { useTranslation } from 'react-i18next';
 import SecondaryButton from "../Buttons/SecondaryButton";
 import SecondaryOutlineButton from "../Buttons/SecondaryButton";
+import generatePrice from '../../Util/generatePrice';
+import { useTranslation } from 'react-i18next';
 
 const MediumPopularBiddingTipio = (props) => {
     const { t } = useTranslation();
     const rangeList = (props.tipio_offer && props.tipio_offer.offer_price_ranges) || [];
-    const firstRange = rangeList.length > 0 && rangeList[0];
-    // const secondRange = rangeList.length > 1 && parseInt(rangeList[1].price, 10) && rangeList[1];
+    const firstRange = rangeList.length > 0 && parseInt(rangeList[1].price, 10) && rangeList[1];
+    const secondRange = rangeList.length > 0 && parseInt(rangeList[0].price, 10) && rangeList[0];
 
     return (
         <MediumPopularTipio {...props}>
             <div className="mediumPopularTipio__content__price">
                 <h6 className="mediumPopularTipio__content__price--marketprice">{props.market_price} Kr</h6>
-                <h6 className="mediumPopularTipio__content__price--saleprice">{firstRange.price} Kr</h6>
+                <h6 className="mediumPopularTipio__content__price--saleprice">{generatePrice(props.bind_count, firstRange, secondRange)} Kr</h6>
             </div>
 
             <div className="mediumPopularTipio__content__submit">
