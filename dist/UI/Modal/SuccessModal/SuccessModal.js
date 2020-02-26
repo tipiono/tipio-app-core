18e15fdb78ab3554a5a43b24ec619a4b8cdfc0a5
ismajl <helloismajl@gmail.com>
2020-02-26 20:49:26 +0100

    added children to success modal

--- dist/UI/Modal/SuccessModal/SuccessModal.js
+++ dist/UI/Modal/SuccessModal/SuccessModal.js
@@ -1,15 +1,19 @@
 "use strict";
 
+function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
+
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
 exports.default = void 0;
 
-var _react = _interopRequireDefault(require("react"));
+var _react = _interopRequireWildcard(require("react"));
 
 var _Modal = require("../Modal");
 
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+function _getRequireWildcardCache() { if (typeof WeakMap !== "function") return null; var cache = new WeakMap(); _getRequireWildcardCache = function _getRequireWildcardCache() { return cache; }; return cache; }
+
+function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } if (obj === null || _typeof(obj) !== "object" && typeof obj !== "function") { return { default: obj }; } var cache = _getRequireWildcardCache(); if (cache && cache.has(obj)) { return cache.get(obj); } var newObj = {}; var hasPropertyDescriptor = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) { var desc = hasPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : null; if (desc && (desc.get || desc.set)) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } newObj.default = obj; if (cache) { cache.set(obj, newObj); } return newObj; }
 
 function _objectWithoutProperties(source, excluded) { if (source == null) return {}; var target = _objectWithoutPropertiesLoose(source, excluded); var key, i; if (Object.getOwnPropertySymbols) { var sourceSymbolKeys = Object.getOwnPropertySymbols(source); for (i = 0; i < sourceSymbolKeys.length; i++) { key = sourceSymbolKeys[i]; if (excluded.indexOf(key) >= 0) continue; if (!Object.prototype.propertyIsEnumerable.call(source, key)) continue; target[key] = source[key]; } } return target; }
 
@@ -20,7 +24,8 @@ function SuccessModal(_ref) {
       successDescription = _ref.successDescription,
       animationURL = _ref.animationURL,
       videoURL = _ref.videoURL,
-      props = _objectWithoutProperties(_ref, ["successTitle", "successDescription", "animationURL", "videoURL"]);
+      children = _ref.children,
+      props = _objectWithoutProperties(_ref, ["successTitle", "successDescription", "animationURL", "videoURL", "children"]);
 
   var renderVideo = function renderVideo() {
     return _react.default.createElement("video", {
@@ -49,7 +54,7 @@ function SuccessModal(_ref) {
     className: "successModal--title"
   }, successTitle), _react.default.createElement("p", {
     className: "successModal--description"
-  }, successDescription))));
+  }, successDescription)), children && children));
 }
 
 var _default = SuccessModal;
