3119fe38e55293c61330683b3ee5b46b960c0fe3
Valmira <valmira.gllareva@ornio.no>
2020-04-21 16:46:51 +0200

    Added component and style for Customer List

--- dist/Company/Cards/OrderItemCard/OrderFilters.js
+++ dist/Company/Cards/OrderItemCard/OrderFilters.js
@@ -1,11 +1,13 @@
 "use strict";
 
+function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
+
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
 exports.default = void 0;
 
-var _react = _interopRequireDefault(require("react"));
+var _react = _interopRequireWildcard(require("react"));
 
 var _ = require("../../..");
 
@@ -13,7 +15,19 @@ var _classnames = _interopRequireDefault(require("classnames"));
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
+function _getRequireWildcardCache() { if (typeof WeakMap !== "function") return null; var cache = new WeakMap(); _getRequireWildcardCache = function _getRequireWildcardCache() { return cache; }; return cache; }
+
+function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } if (obj === null || _typeof(obj) !== "object" && typeof obj !== "function") { return { default: obj }; } var cache = _getRequireWildcardCache(); if (cache && cache.has(obj)) { return cache.get(obj); } var newObj = {}; var hasPropertyDescriptor = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) { var desc = hasPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : null; if (desc && (desc.get || desc.set)) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } newObj.default = obj; if (cache) { cache.set(obj, newObj); } return newObj; }
+
 function _extends() { _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; }; return _extends.apply(this, arguments); }
+
+function _slicedToArray(arr, i) { return _arrayWithHoles(arr) || _iterableToArrayLimit(arr, i) || _nonIterableRest(); }
+
+function _nonIterableRest() { throw new TypeError("Invalid attempt to destructure non-iterable instance"); }
+
+function _iterableToArrayLimit(arr, i) { if (!(Symbol.iterator in Object(arr) || Object.prototype.toString.call(arr) === "[object Arguments]")) { return; } var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"] != null) _i["return"](); } finally { if (_d) throw _e; } } return _arr; }
+
+function _arrayWithHoles(arr) { if (Array.isArray(arr)) return arr; }
 
 var SortIcon = function SortIcon() {
   return _react.default.createElement("svg", {
@@ -31,10 +45,26 @@ var SortIcon = function SortIcon() {
 };
 
 var OrderFilters = function OrderFilters(props) {
-  return _react.default.createElement(_react.default.Fragment, null, _react.default.createElement("thead", {
+  var _useState = (0, _react.useState)('Leveringsstatus'),
+      _useState2 = _slicedToArray(_useState, 2),
+      title = _useState2[0],
+      setTitle = _useState2[1];
+
+  (0, _react.useEffect)(function () {
+    if (window.innerWidth <= 768) {
+      setTitle('Status');
+    } else {
+      setTitle('Leveringsstatus');
+    }
+  }, []);
+  return _react.default.createElement(_react.default.Fragment, null, _react.default.createElement("div", {
     className: "orderFilters"
-  }, _react.default.createElement("tr", null, _react.default.createElement("th", {
-    className: "orderFilters--dropdown"
+  }, _react.default.createElement("div", {
+    className: (0, _classnames.default)('orderFilters--dropdown', {
+      'orderFilters__active': props.visible
+    })
+  }, _react.default.createElement("span", {
+    className: "orderFilters--dropdown--icon"
   }, _react.default.createElement("svg", {
     className: "mr-2",
     width: 27,
@@ -45,9 +75,11 @@ var OrderFilters = function OrderFilters(props) {
     strokeWidth: 0.2,
     fill: "#5C6265",
     fillRule: "evenodd"
-  })), _react.default.createElement(_.DropDown, _extends({
-    title: 'Leveringsstatus'
-  }, props), _react.default.createElement(_.DropDownItem, null, _react.default.createElement("a", {
+  }))), _react.default.createElement(_.DropDown, _extends({
+    title: title
+  }, props, {
+    showCloseIcon: true
+  }), _react.default.createElement(_.DropDownItem, null, _react.default.createElement("a", {
     href: "/",
     onClick: function onClick(e) {
       e.preventDefault();
@@ -59,7 +91,9 @@ var OrderFilters = function OrderFilters(props) {
       e.preventDefault();
       props.onClick(2);
     }
-  }, "Merk alle som ikke levert")))), _react.default.createElement("th", null, _react.default.createElement("a", {
+  }, "Merk alle som ikke levert")))), _react.default.createElement("div", {
+    className: "w-25"
+  }, _react.default.createElement("a", {
     className: (0, _classnames.default)('orderFilters--sort', {
       active: props.order_by === 'full_name'
     }),
@@ -70,8 +104,12 @@ var OrderFilters = function OrderFilters(props) {
     }
   }, _react.default.createElement("span", {
     className: "mr-2"
-  }, "Navn"), _react.default.createElement(SortIcon, null))), _react.default.createElement("th", null, "Adresse"), _react.default.createElement("th", null, _react.default.createElement("a", {
-    className: (0, _classnames.default)('orderFilters--sort', {
+  }, "Navn"), _react.default.createElement(SortIcon, null))), _react.default.createElement("div", {
+    className: "orderFilters__addrese w-25"
+  }, "Adresse"), _react.default.createElement("div", {
+    className: "w-25"
+  }, _react.default.createElement("a", {
+    className: (0, _classnames.default)('orderFilters--sort lastElement', {
       active: props.order_by === 'zip_code'
     }),
     href: "",
@@ -81,7 +119,7 @@ var OrderFilters = function OrderFilters(props) {
     }
   }, _react.default.createElement("span", {
     className: "mr-2"
-  }, "Post sted"), _react.default.createElement(SortIcon, null))), _react.default.createElement("th", null, _react.default.createElement("br", null)))));
+  }, "Post sted"), _react.default.createElement(SortIcon, null)))));
 };
 
 var _default = OrderFilters;
