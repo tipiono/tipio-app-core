3843b58ef0aba699c07ac35158b408fb37b54d10
Flamur Mavraj <flamur.mavraj@ornio.no>
2020-04-21 22:26:33 +0200

    Added type to cropped image, set autoCropArea to 1 = 100%

--- dist/Customer/ImageUploader/ImageUploader.js
+++ dist/Customer/ImageUploader/ImageUploader.js
@@ -29,6 +29,14 @@ function _getRequireWildcardCache() { if (typeof WeakMap !== "function") return 
 
 function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } if (obj === null || _typeof(obj) !== "object" && typeof obj !== "function") { return { default: obj }; } var cache = _getRequireWildcardCache(); if (cache && cache.has(obj)) { return cache.get(obj); } var newObj = {}; var hasPropertyDescriptor = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) { var desc = hasPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : null; if (desc && (desc.get || desc.set)) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } newObj.default = obj; if (cache) { cache.set(obj, newObj); } return newObj; }
 
+function _toConsumableArray(arr) { return _arrayWithoutHoles(arr) || _iterableToArray(arr) || _nonIterableSpread(); }
+
+function _nonIterableSpread() { throw new TypeError("Invalid attempt to spread non-iterable instance"); }
+
+function _iterableToArray(iter) { if (Symbol.iterator in Object(iter) || Object.prototype.toString.call(iter) === "[object Arguments]") return Array.from(iter); }
+
+function _arrayWithoutHoles(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = new Array(arr.length); i < arr.length; i++) { arr2[i] = arr[i]; } return arr2; } }
+
 function _slicedToArray(arr, i) { return _arrayWithHoles(arr) || _iterableToArrayLimit(arr, i) || _nonIterableRest(); }
 
 function _nonIterableRest() { throw new TypeError("Invalid attempt to destructure non-iterable instance"); }
@@ -126,12 +134,14 @@ function ImageUploader(_ref) {
     }
 
     var tempImage = files[croppedImageIndex];
-    var file = new File([blob], tempImage.name);
+    var file = new File([blob], tempImage.name, {
+      type: tempImage.type
+    });
     file.id = tempImage.id;
     file.path = tempImage.path;
     file.preview = URL.createObjectURL(file);
     files[croppedImageIndex] = file;
-    setFiles(files);
+    setFiles(_toConsumableArray(files));
     setFieldValue('files', files);
 
     if (!icc && !initialCropCompleted) {
