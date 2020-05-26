01e012e1de07642c0b61d25de8e457a94bb98d9d
Valmira <valmira.gllareva@ornio.no>
2020-05-26 12:13:42 +0200

    Made some changes in generatePrice function

--- src/lib/Util/generatePrice.js
+++ src/lib/Util/generatePrice.js
@@ -2,14 +2,13 @@ export default (bindCount, range1, range2) => {
     let firstRange = {};
     let secondRange = {};
     if ((range1 && range1.id) < (range2 && range2.id)) {
-      firstRange = range1;
-      secondRange = range2;
+        firstRange = range1;
+        secondRange = range2;
     } else {
-      firstRange = range2;
-      secondRange = range1;
+        firstRange = range2;
+        secondRange = range1;
     }
-    return parseInt(bindCount, 10) >= 0
-        && parseInt(bindCount, 10) <= parseInt(firstRange.to, 10)
-        ? firstRange.price
-        : secondRange.price;
-}
+    return parseInt(bindCount, 10) >= 0 && parseInt(bindCount, 10) <= parseInt(firstRange && firstRange.to, 10)
+        ? firstRange && firstRange.price
+        : secondRange && secondRange.price;
+};
