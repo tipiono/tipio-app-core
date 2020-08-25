9c547c83f1b04c01288cbfe80b5e169f9c94c2bd
Muhamet Humolli <muhamet.humolli@ornio.no>
2020-08-25 14:18:32 +0200

    fix number of interested at highlighted voting

--- src/lib/UI/MediumPopularTipio/MediumPopularVotingTipioEdit.js
+++ src/lib/UI/MediumPopularTipio/MediumPopularVotingTipioEdit.js
@@ -5,7 +5,7 @@ import SecondaryOutlineButton from '../Buttons/SecondaryOutlineButton';
 const MediumPopularVotingTipioEdit = (props) => {
     return (
         <MediumPopularTipio {...props}>
-            <h6 className="mediumPopularTipio__content--interested">2 789 interesserte!</h6>
+            <h6 className="mediumPopularTipio__content--interested">{props.count} interesserte!</h6>
 
             <div className="mediumPopularTipio__content__submit">
                 <SecondaryOutlineButton text="Rediger" onClick={props.onEditClick} />
