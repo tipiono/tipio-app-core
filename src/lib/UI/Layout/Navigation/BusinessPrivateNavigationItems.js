016e7dedacd155e644c8dd87e60e143dafb7ad2b
Valmira <valmira.gllareva@ornio.no>
2020-09-21 11:47:09 +0200

    Main menu changed

--- src/lib/UI/Layout/Navigation/BusinessPrivateNavigationItems.js
+++ src/lib/UI/Layout/Navigation/BusinessPrivateNavigationItems.js
@@ -2,14 +2,16 @@ import React from 'react';
 
 function BusinessPrivateNavigationItems(props) {
     return (
-       <ul className="nav-list d-none d-md-block">
-           <li className="nav-list-item">
-               <a className="nav-list-link nav-list-lg-link" href="#" onClick={props.mainNavOnClick}>Privat</a>
-           </li>
-           <li className="nav-list-item">
-               <a className="nav-list-link nav-list-lg-link active" href="#">Bedrift</a>
-           </li>
-       </ul>
-    )
+        <ul className="nav-list d-none d-md-block">
+            <li className="nav-list-item">
+                <button className="link-button nav-list-link nav-list-lg-link " onClick={props.mainNavOnClick}>
+                    Privat
+                </button>
+            </li>
+            <li className="nav-list-item">
+                <button className="link-button nav-list-link nav-list-lg-link active">Bedrift</button>
+            </li>
+        </ul>
+    );
 }
 export default BusinessPrivateNavigationItems;
