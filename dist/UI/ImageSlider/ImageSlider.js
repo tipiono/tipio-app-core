62533a5973336ed32664cf1b0b5bb586f32a9253
Valmira <valmira.gllareva@ornio.no>
2020-10-22 13:17:11 +0200

    Refactor image slider to support Video

--- dist/UI/ImageSlider/ImageSlider.js
+++ dist/UI/ImageSlider/ImageSlider.js
@@ -9,6 +9,8 @@ var _react = _interopRequireDefault(require("react"));
 
 var _reactResponsiveCarousel = require("react-responsive-carousel");
 
+var _reactPlayer = _interopRequireDefault(require("react-player"));
+
 var _classnames = _interopRequireDefault(require("classnames"));
 
 require("react-responsive-carousel/lib/styles/carousel.min.css");
@@ -17,12 +19,54 @@ var _TipioCountdown = _interopRequireDefault(require("../TipioCountdown/TipioCou
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-function ImageSlider(_ref) {
+var ImageSlider = function ImageSlider(_ref) {
   var images = _ref.images,
       showThumbs = _ref.showThumbs,
       showTimer = _ref.showTimer,
       tipio_expires_in = _ref.tipio_expires_in,
-      onClick = _ref.onClick;
+      onClick = _ref.onClick,
+      _ref$hasVideo = _ref.hasVideo,
+      hasVideo = _ref$hasVideo === void 0 ? false : _ref$hasVideo;
+
+  var YoutubeSlide = function YoutubeSlide(_ref2) {
+    var url = _ref2.url,
+        isSelected = _ref2.isSelected;
+    return /*#__PURE__*/_react.default.createElement(_reactPlayer.default, {
+      width: "100%",
+      url: url,
+      playing: isSelected
+    });
+  };
+
+  var getVideoThumb = function getVideoThumb(videoId) {
+    return "https://img.youtube.com/vi/".concat(videoId, "/default.jpg");
+  };
+
+  var getVideoId = function getVideoId(url) {
+    return url.substr('https://www.youtube.com/watch?v='.length, url.length);
+  };
+
+  var imageSrc = [];
+  images.map(function (image) {
+    imageSrc.push(image.blob_url);
+  });
+
+  if (hasVideo) {
+    var videoId = getVideoId('https://www.youtube.com/watch?v=VeT16PSVjLE');
+
+    if (videoId) {
+      imageSrc.push(getVideoThumb(videoId));
+    }
+  }
+
+  var customRenderThumb = function customRenderThumb() {
+    return imageSrc.map(function (item) {
+      return /*#__PURE__*/_react.default.createElement("img", {
+        src: item
+      });
+    });
+  };
+
   return /*#__PURE__*/_react.default.createElement(_react.default.Fragment, null, /*#__PURE__*/_react.default.createElement("div", {
     className: (0, _classnames.default)('imageSlider', images && {
       showIndicators: images.length >= 5
@@ -37,20 +81,24 @@ function ImageSlider(_ref) {
     infiniteLoop: true,
     renderBottomCenterControls: false,
     swipeScrollTolerance: 40,
-    onClickItem: onClick && onClick
+    onClickItem: onClick && onClick,
+    renderThumbs: customRenderThumb
   }, images && images.map(function (item, i) {
     return /*#__PURE__*/_react.default.createElement("img", {
       src: item.blob_url,
       alt: "...",
       key: i
     });
+  }), hasVideo && /*#__PURE__*/_react.default.createElement(YoutubeSlide, {
+    key: "youtube-1",
+    url: "https://www.youtube.com/watch?v=VeT16PSVjLE"
   })), showTimer && tipio_expires_in && /*#__PURE__*/_react.default.createElement("div", {
     className: "imageSlider__countDown"
   }, /*#__PURE__*/_react.default.createElement(_TipioCountdown.default, {
     className: "timer",
     expires_in: tipio_expires_in
   }))));
-}
+};
 
 var _default = ImageSlider;
 exports.default = _default;
--- src/lib/UI/ImageSlider/ImageSlider.js
+++ src/lib/UI/ImageSlider/ImageSlider.js
@@ -1,10 +1,34 @@
 import React from 'react';
 import { Carousel } from 'react-responsive-carousel';
+import ReactPlayer from 'react-player';
 import cx from 'classnames';
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
 import TipioCountdown from '../TipioCountdown/TipioCountdown';
 
-function ImageSlider({ images, showThumbs, showTimer, tipio_expires_in, onClick }) {
+const ImageSlider = ({ images, showThumbs, showTimer, tipio_expires_in, onClick, hasVideo = false }) => {
+    const YoutubeSlide = ({ url, isSelected }) => <ReactPlayer width="100%" url={url} playing={isSelected} />;
+
+    const getVideoThumb = (videoId) => `https://img.youtube.com/vi/${videoId}/default.jpg`;
+
+    const getVideoId = (url) => url.substr('https://www.youtube.com/watch?v='.length, url.length);
+
+    let imageSrc = [];
+    images.map((image) => {
+        imageSrc.push(image.blob_url);
+    });
+    if (hasVideo) {
+        const videoId = getVideoId('https://www.youtube.com/watch?v=VeT16PSVjLE');
+        if (videoId) {
+            imageSrc.push(getVideoThumb(videoId));
+        }
+    }
+
+    const customRenderThumb = () => {
+        return imageSrc.map((item) => {
+            return <img src={item} />;
+        });
+    };
+
     return (
         <>
             <div
@@ -23,8 +47,10 @@ function ImageSlider({ images, showThumbs, showTimer, tipio_expires_in, onClick 
                     renderBottomCenterControls={false}
                     swipeScrollTolerance={40}
                     onClickItem={onClick && onClick}
+                    renderThumbs={customRenderThumb}
                 >
                     {images && images.map((item, i) => <img src={item.blob_url} alt="..." key={i} />)}
+                    {hasVideo && <YoutubeSlide key="youtube-1" url="https://www.youtube.com/watch?v=VeT16PSVjLE" />}
                 </Carousel>
                 {showTimer && tipio_expires_in && (
                     <div className="imageSlider__countDown">
@@ -34,6 +60,6 @@ function ImageSlider({ images, showThumbs, showTimer, tipio_expires_in, onClick 
             </div>
         </>
     );
-}
+};
 
 export default ImageSlider;
