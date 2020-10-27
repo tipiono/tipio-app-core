5c00cd6ded592253cdf4fd004e84d0a56418f4f7
skulsta <sti.sku@gmail.com>
2020-10-27 15:44:01 +0100

    Switch from percentage to NOK if the discount is less than 15 percent

--- dist/Customer/Cards/BiddingCard/BiddingCardShareButton.js
+++ dist/Customer/Cards/BiddingCard/BiddingCardShareButton.js
@@ -3,7 +3,7 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-exports.default = BiddingCardShareButton;
+exports.default = void 0;
 
 var _react = _interopRequireDefault(require("react"));
 
@@ -11,8 +11,13 @@ var _SecondaryButton = _interopRequireDefault(require("../../../UI/Buttons/Secon
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-function BiddingCardShareButton() {
-  return /*#__PURE__*/_react.default.createElement(_SecondaryButton.default, {
+function _extends() { _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; }; return _extends.apply(this, arguments); }
+
+var BiddingCardShareButton = function BiddingCardShareButton(props) {
+  return /*#__PURE__*/_react.default.createElement(_SecondaryButton.default, _extends({
     text: "Del n\xE5"
-  });
-}
+  }, props));
+};
+
+var _default = BiddingCardShareButton;
+exports.default = _default;
