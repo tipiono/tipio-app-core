b075383015cfb0eb3ea853134949df2e75521406
skulsta <sti.sku@gmail.com>
2020-10-29 18:55:25 +0100

    Doing some cleaning

--- src/lib/UI/MediumPopularTipio/MediumPopularBiddingTipio.js
+++ src/lib/UI/MediumPopularTipio/MediumPopularBiddingTipio.js
@@ -2,27 +2,16 @@ import React from 'react';
 import MediumPopularTipio from './MediumPopularTipio';
 import SecondaryButton from '../Buttons/SecondaryButton';
 import SecondaryOutlineButton from '../Buttons/SecondaryButton';
-import replaceWithSpace from '../../Util/replaceWithSpace';
+import { getBestPrice } from '../../Util/calculateDiscountPercentage';
 
 const MediumPopularBiddingTipio = (props) => {
-    const rangeList = (props.tipio_offer && props.tipio_offer.offer_price_ranges) || [];
-    const secondRange = rangeList.length > 0 && parseInt(rangeList[0].price, 10) && rangeList[0];
-
-    let bestPrice = null;
-    if (rangeList.length > 0) {
-        bestPrice =
-            parseInt(rangeList[0].price) < parseInt(rangeList[1].price)
-                ? parseInt(rangeList[0].price)
-                : parseInt(rangeList[1].price);
-    }
-
     return (
-        <MediumPopularTipio {...props} bestPrice={bestPrice}>
+        <MediumPopularTipio {...props}>
             <div className="mediumPopularTipio__content__price">
                 <h6 className="mediumPopularTipio__content__price--marketprice">{props.market_price} Kr</h6>
                 <h6 className="mediumPopularTipio__content__price--saleprice">
                     {' '}
-                    {bestPrice && bestPrice.toLocaleString('nb-NO')} kr
+                    {getBestPrice(props.tipio_offer.offer_price_ranges).toLocaleString('nb-NO')} kr
                 </h6>
             </div>
 
